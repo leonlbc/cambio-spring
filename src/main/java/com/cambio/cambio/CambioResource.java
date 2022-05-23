fe7e4e6692fee47d7bd10fc88dc152d07a22ee79
Leon <cohenleonlbc@gmail.com>
2022-05-23 10:49:16 -0300

    moeda entity

---
+++ src/main/java/com/cambio/cambio/CambioResource.java
@@ -0,0 +1,35 @@
+package com.cambio.cambio;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CambioResource {
+
+@Autowired
+private MoedaService moedaService;
+
+    
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return "Hello ESPM! :)";
+    }
+
+
+    @GetMapping("/moeda")
+    public List<Moeda> listMoeda(){
+        return moedaService.listAll();        
+    }
+
+    @PostMapping("/moeda")
+    public void save(@RequestBody Moeda moeda){
+        moedaService.create(moeda);
+    }
+
+    
+}
